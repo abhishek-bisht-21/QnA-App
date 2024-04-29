a846c2518d52806d95ee8dde7799b648e7dfee09
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-04-30 00:51:22 +0530

    Added Sidebar component and styling

--- frontend/src/components/Quora.js
+++ frontend/src/components/Quora.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import QuoraHeader from './QuoraHeader'
+import Feed from "./Feed";
+import Sidebar from "./Sidebar";
+import Widget from "./Widget";
+import "./css/Quora.css";
 
 function Quora() {
   return (
     <div className='quora'>
-	<QuoraHeader/>
+      <QuoraHeader />
+      <div className='quora__contents'>
+        <div className='quora__content'>
+            <Sidebar/>
+            <Feed/>
+            <Widget/>
+        </div>
+      </div>
     </div>
   )
 }
