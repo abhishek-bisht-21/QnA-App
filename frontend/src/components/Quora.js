34cc55fd1149130a41d66158bf0cb9a62f4ff168
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-04-30 00:35:36 +0530

    Added Header Component

---
+++ frontend/src/components/Quora.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import QuoraHeader from './QuoraHeader'
+
+function Quora() {
+  return (
+    <div className='quora'>
+	<QuoraHeader/>
+    </div>
+  )
+}
+
+export default Quora
