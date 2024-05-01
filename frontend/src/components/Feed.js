aa7d07d67cd96c771dcd36c9648970df3f8a8c0d
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-05-01 21:29:35 +0530

    Added Feed quoraBox and Post component along with their styling

--- frontend/src/components/Feed.js
+++ frontend/src/components/Feed.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import QuoraBox from './QuoraBox'
+import './css/Feed.css'
 
 function Feed() {
   return (
-    <div>Feed</div>
+    <div className='feed'><QuoraBox/></div>
   )
 }
 
